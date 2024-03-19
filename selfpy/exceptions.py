9c6fe006c4d669bb606b56129c365ec101b542f5
EletrixTime <71174682+EletrixtimeYT@users.noreply.github.com>
2024-03-19 14:18:49 +0100

    0.0.1

---
+++ selfpy/exceptions.py
@@ -0,0 +1,10 @@
+class Invalid_Token(Exception):
+    pass
+class not_found(Exception):
+    pass
+class invalid(Exception):
+    pass
+class unknow_error(Exception):
+    pass
+class ratelimit(Exception):
+    pass
